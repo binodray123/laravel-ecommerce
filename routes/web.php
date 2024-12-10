eb23589cc46e127ee1afc233b5a9e7a5139d27da
Binod <ybinod857@gmail.com>
2024-12-10 20:10:26 +0545

    Created Admin and User Authentication in Laravel E-Commerce Project.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +21,12 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware('auth', AuthAdmin::class)->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
