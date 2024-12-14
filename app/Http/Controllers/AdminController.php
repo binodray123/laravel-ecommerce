a62ea5e9601e6c0eeb1ab26b3ac75579c7de3293
Binod <ybinod857@gmail.com>
2024-12-14 13:46:56 +0545

    Created Model and Migration for categories table and added admin category page with pagination links

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Carbon\Carbon;
@@ -116,5 +117,11 @@ class AdminController extends Controller
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with('status', 'Brand has been deleted successfully!');
+    }
+
+    public function categories()
+    {
+        $categories = Category::orderBy('id','DESC')->paginate(10);
+        return view('admin.categories',compact('categories'));
     }
 }
