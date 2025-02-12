a665e50105f61c527e453b9f72a0341e2ffd24cd
Binod <ybinod857@gmail.com>
2025-02-12 18:36:50 +0545

    Show Users Order and Order Details

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::post('/wishlist/move-to-cart/{rowId}', [WishlistController::class, 'move_
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+    Route::get('/account-orders', [UserController::class, 'orders'])->name('user.orders');
+    Route::get('/account-order/{order_id}/details', [UserController::class, 'order_details'])->name('user.order.details');
 });
 
 Route::middleware('auth', AuthAdmin::class)->group(function () {
